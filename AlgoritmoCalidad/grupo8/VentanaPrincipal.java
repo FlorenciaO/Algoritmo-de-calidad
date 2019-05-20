430f923a028399ef7b87c3e37effe0de17465af3
Florencia <o.florencia123@gmail.com>
2019-05-20 12:35:38 -0300

    Cambiando formato de letra a los labels

--- AlgoritmoCalidad/grupo8/VentanaPrincipal.java
+++ AlgoritmoCalidad/grupo8/VentanaPrincipal.java
@@ -5,6 +5,7 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Font;
 
 public class VentanaPrincipal extends JFrame {
 	
@@ -15,30 +16,37 @@ public class VentanaPrincipal extends JFrame {
 		getContentPane().setLayout(null);
 		
 		JLabel lblCaractersticasAEvaluar = new JLabel("Características a evaluar:");
-		lblCaractersticasAEvaluar.setBounds(39, 26, 162, 16);
+		lblCaractersticasAEvaluar.setFont(new Font("Lucida Grande", Font.BOLD, 16));
+		lblCaractersticasAEvaluar.setBounds(39, 26, 208, 16);
 		getContentPane().add(lblCaractersticasAEvaluar);
 		
-		JLabel lblFuncionabilidad = new JLabel("Funcionabilidad");
-		lblFuncionabilidad.setBounds(82, 80, 119, 16);
+		JLabel lblFuncionabilidad = new JLabel("· Funcionabilidad");
+		lblFuncionabilidad.setFont(new Font("Lucida Grande", Font.ITALIC, 15));
+		lblFuncionabilidad.setBounds(82, 80, 130, 16);
 		getContentPane().add(lblFuncionabilidad);
 		
-		JLabel lblEficiencia = new JLabel("Eficiencia");
+		JLabel lblEficiencia = new JLabel("· Eficiencia");
+		lblEficiencia.setFont(new Font("Lucida Grande", Font.ITALIC, 15));
 		lblEficiencia.setBounds(82, 130, 119, 16);
 		getContentPane().add(lblEficiencia);
 		
-		JLabel lblFiabilidad = new JLabel("Fiabilidad");
+		JLabel lblFiabilidad = new JLabel("· Fiabilidad");
+		lblFiabilidad.setFont(new Font("Lucida Grande", Font.ITALIC, 15));
 		lblFiabilidad.setBounds(82, 180, 119, 16);
 		getContentPane().add(lblFiabilidad);
 		
-		JLabel lblMantenibilidad = new JLabel("Mantenibilidad");
+		JLabel lblMantenibilidad = new JLabel("· Mantenibilidad");
+		lblMantenibilidad.setFont(new Font("Lucida Grande", Font.ITALIC, 15));
 		lblMantenibilidad.setBounds(82, 232, 119, 16);
 		getContentPane().add(lblMantenibilidad);
 		
-		JLabel lblUsabilidad = new JLabel("Usabilidad");
+		JLabel lblUsabilidad = new JLabel("· Usabilidad");
+		lblUsabilidad.setFont(new Font("Lucida Grande", Font.ITALIC, 15));
 		lblUsabilidad.setBounds(82, 282, 119, 16);
 		getContentPane().add(lblUsabilidad);
 		
-		JLabel lblPortabilidad = new JLabel("Portabilidad");
+		JLabel lblPortabilidad = new JLabel("· Portabilidad");
+		lblPortabilidad.setFont(new Font("Lucida Grande", Font.ITALIC, 15));
 		lblPortabilidad.setBounds(82, 334, 119, 16);
 		getContentPane().add(lblPortabilidad);
 		
@@ -55,6 +63,7 @@ public class VentanaPrincipal extends JFrame {
 	}
 	
 	public void comenzar() {
+		dispose();
 		new Funcionabilidad(this).setVisible(true);
 	}
 
