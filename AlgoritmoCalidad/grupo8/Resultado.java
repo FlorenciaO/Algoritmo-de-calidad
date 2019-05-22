0861bc1cbd0f852d790d3354bfe87138105c0344
Florencia <o.florencia123@gmail.com>
2019-05-22 14:32:03 -0300

    Creacion de toString para la muestra por pantalla

--- AlgoritmoCalidad/grupo8/Resultado.java
+++ AlgoritmoCalidad/grupo8/Resultado.java
@@ -3,5 +3,19 @@ package grupo8;
 public enum Resultado {
 	
 	Mala, Buena, Excelente, RazonablementeAceptable;
-
+	
+	public String toString() {
+		switch(this) {
+		case Mala:
+			return "Mala";
+		case Buena:
+			return "Buena";
+		case Excelente:
+			return "Excelente";
+		case RazonablementeAceptable:
+			return "Razonablemente Aceptable";
+		default:
+			return null;
+		}
+	}
 }
