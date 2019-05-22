3eefb0ed9079744563c5868b98a34151d0af1447
Florencia <o.florencia123@gmail.com>
2019-05-22 11:33:38 -0300

    Modificando la clase

--- AlgoritmoCalidad/grupo8/Funcionabilidad.java
+++ AlgoritmoCalidad/grupo8/Funcionabilidad.java
@@ -2,9 +2,16 @@ package grupo8;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Funcionabilidad extends JFrame{
 
@@ -14,6 +21,8 @@ public class Funcionabilidad extends JFrame{
 	private static final long serialVersionUID = -5745267259821566766L;
 	private JTextField txtInteroperabilidad;
 	private JTextField txtExactitud;
+	private static final String EMPTY_STRING = "";
+	private Resultado resultado;
 	
 	public Funcionabilidad(VentanaPrincipal ventanaPrincipal) {
 		setTitle("Característica: Funcionabilidad");
@@ -34,7 +43,7 @@ public class Funcionabilidad extends JFrame{
 		
 		JLabel lblExactitudEnLos = new JLabel("Exactitud en los resultados");
 		lblExactitudEnLos.setFont(new Font("Lucida Grande", Font.ITALIC, 15));
-		lblExactitudEnLos.setBounds(20, 329, 474, 16);
+		lblExactitudEnLos.setBounds(20, 338, 474, 16);
 		getContentPane().add(lblExactitudEnLos);
 		
 		JLabel lbl1 = new JLabel("- ¿Con cuántos sistemas se relaciona su producto software?");
@@ -45,11 +54,17 @@ public class Funcionabilidad extends JFrame{
 		lbl2.setBounds(48, 158, 378, 16);
 		getContentPane().add(lbl2);
 		
-		JLabel label_1 = new JLabel("- ¿Con cuántos sistemas se relaciona su producto software?");
-		label_1.setBounds(48, 357, 428, 16);
-		getContentPane().add(label_1);
+		JLabel lbl8 = new JLabel("- ¿Con cuántos sistemas se relaciona su producto software?");
+		lbl8.setBounds(48, 366, 428, 16);
+		getContentPane().add(lbl8);
 		
 		txtInteroperabilidad = new JTextField();
+		txtInteroperabilidad.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				validarIngreso(e, txtInteroperabilidad);
+			}
+		});
 		txtInteroperabilidad.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
 		txtInteroperabilidad.setToolTipText("Ingrese cantidad total de sistemas con los que se relaciona su sistema");
 		txtInteroperabilidad.setBounds(58, 86, 130, 26);
@@ -57,10 +72,16 @@ public class Funcionabilidad extends JFrame{
 		txtInteroperabilidad.setColumns(10);
 		
 		txtExactitud = new JTextField();
+		txtExactitud.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				validarIngreso(e, txtExactitud);
+			}
+		});
 		txtExactitud.setToolTipText("Ingrese cantidad total de sistemas con los que se relaciona su sistema");
 		txtExactitud.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
 		txtExactitud.setColumns(10);
-		txtExactitud.setBounds(48, 381, 130, 26);
+		txtExactitud.setBounds(48, 394, 130, 26);
 		getContentPane().add(txtExactitud);
 		
 		JLabel lbl3 = new JLabel("- Mi software tiene ocultamiento de contraseña al ingresar");
@@ -79,28 +100,82 @@ public class Funcionabilidad extends JFrame{
 		lbl6.setBounds(48, 271, 378, 16);
 		getContentPane().add(lbl6);
 		
-		JCheckBox checkBox = new JCheckBox("");
-		checkBox.setBounds(435, 154, 28, 23);
-		getContentPane().add(checkBox);
+		JCheckBox checkBox1 = new JCheckBox("");
+		checkBox1.setBounds(435, 154, 28, 23);
+		getContentPane().add(checkBox1);
 		
-		JCheckBox checkBox_1 = new JCheckBox("");
-		checkBox_1.setBounds(435, 182, 28, 23);
-		getContentPane().add(checkBox_1);
+		JCheckBox checkBox2 = new JCheckBox("");
+		checkBox2.setBounds(435, 182, 28, 23);
+		getContentPane().add(checkBox2);
 		
-		JCheckBox checkBox_2 = new JCheckBox("");
-		checkBox_2.setBounds(435, 208, 28, 23);
-		getContentPane().add(checkBox_2);
+		JCheckBox checkBox3 = new JCheckBox("");
+		checkBox3.setBounds(435, 208, 28, 23);
+		getContentPane().add(checkBox3);
 		
-		JCheckBox checkBox_3 = new JCheckBox("");
-		checkBox_3.setBounds(435, 236, 28, 23);
-		getContentPane().add(checkBox_3);
+		JCheckBox checkBox4 = new JCheckBox("");
+		checkBox4.setBounds(435, 236, 28, 23);
+		getContentPane().add(checkBox4);
 		
-		JCheckBox checkBox_4 = new JCheckBox("");
-		checkBox_4.setBounds(435, 280, 28, 23);
-		getContentPane().add(checkBox_4);
+		JCheckBox checkBox5 = new JCheckBox("");
+		checkBox5.setBounds(435, 280, 28, 23);
+		getContentPane().add(checkBox5);
 		
-		JLabel lblDespusDe = new JLabel("después de 15 minutos");
-		lblDespusDe.setBounds(58, 287, 378, 16);
-		getContentPane().add(lblDespusDe);
+		JLabel lbl7 = new JLabel("después de 15 minutos");
+		lbl7.setBounds(58, 287, 378, 16);
+		getContentPane().add(lbl7);
+		
+		JButton btnEvaluar = new JButton("Evaluar");
+		btnEvaluar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				evaluar();
+			}
+		});
+		btnEvaluar.setBounds(20, 432, 117, 29);
+		getContentPane().add(btnEvaluar);
+		
+		JLabel lblEvInteroperabilidad = new JLabel("");
+		lblEvInteroperabilidad.setBounds(217, 91, 61, 16);
+		getContentPane().add(lblEvInteroperabilidad);
+		
+		JLabel lblEvSegAcceso = new JLabel("");
+		lblEvSegAcceso.setBounds(48, 315, 61, 16);
+		getContentPane().add(lblEvSegAcceso);
+		
+		JLabel lblEvExacResultados = new JLabel("");
+		lblEvExacResultados.setBounds(217, 400, 61, 16);
+		getContentPane().add(lblEvExacResultados);
+		
+		JButton btnSiguiente = new JButton("Siguiente");
+		btnSiguiente.setToolTipText("Debe evaluar la característica antes de seguir");
+		btnSiguiente.setEnabled(false);
+		btnSiguiente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				siguiente();
+			}
+		});
+		btnSiguiente.setBounds(359, 432, 117, 29);
+		getContentPane().add(btnSiguiente);
+		
+		JLabel lblEvFuncionalidad = new JLabel("");
+		lblEvFuncionalidad.setBounds(149, 437, 61, 16);
+		getContentPane().add(lblEvFuncionalidad);
+	}
+	
+	private void validarIngreso(KeyEvent e, JTextField txt) {
+		int key = e.getKeyCode();
+		
+		if(!((key>= 48 && key<=57) || key == KeyEvent.VK_BACK_SPACE)) {
+			JOptionPane.showMessageDialog(this, "Debe ingresar un número", "Ingreso inválido", JOptionPane.WARNING_MESSAGE);
+			txt.setText(EMPTY_STRING);
+		}	
+	}
+	
+	private void siguiente() {
+		
+	}
+	
+	private void evaluar() {
+		
+		
 	}
 }
